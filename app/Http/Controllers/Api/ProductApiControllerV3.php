e865240788fbfad4ba61dc2f30f84ba9133312b1
JuanM0412 <juangomezp1204@gmail.com>
2024-03-22 07:50:26 -0500

    API system

---
+++ app/Http/Controllers/Api/ProductApiControllerV3.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductCollection;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+
+class ProductApiControllerV3 extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $products = new ProductCollection(Product::all());
+
+        return response()->json($products, 200);
+    }
+
+    public function paginate(): JsonResponse
+    {
+        $products = new ProductCollection(Product::paginate(5));
+
+        return response()->json($products, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index')->name('api.product.index');
+Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductApiController@show')->name('api.product.show');
+Route::get('/v2/products', 'App\Http\Controllers\Api\ProductApiControllerV2@index')->name('api.v2.product.index');
+Route::get('/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiControllerV2@show')->name('api.v2.product.show');
+Route::get('/v3/products', 'App\Http\Controllers\Api\ProductApiControllerV3@index')->name('api.v3.product.index');
+Route::get('/v3/products/paginate', 'App\Http\Controllers\Api\ProductApiControllerV3@paginate')->name('api.v3.product.paginate');
